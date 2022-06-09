501e611b83c4d86aded7452063e2b2d02e08b8b0
Dmitriy Shishkin <6ast3r@gmail.com>
2022-06-09 20:10:55 +0700

    Add fr-FR

---
+++ localization/fr-FR.ts
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+
+import {
+  strings as enStrings,
+} from './en-US';
+
+const dateFormat = 'DD MM YYYY';
+
+const supportUrl = 'https://support.my-site.com';
+const localizedSupportUrl = `${supportUrl}/fr-fr/`;
+const articlesUrl = `${localizedSupportUrl}articles/`;
+const supportNewRequestUrl = `${localizedSupportUrl}new`;
+
+const getCommonAddressTitle = (vehicleName: string) => `Votre ${vehicleName} adresse`;
+
+export const strings: typeof enStrings = {
+  bookTitle: 'Livre',
+
+  commonErrorMessage: "Oups, quelque chose s'est mal passé",
+
+  supportNewRequestUrl,
+
+  dateFormats: {
+    date: dateFormat,
+    dateTime: `${dateFormat} HH:mm`,
+  },
+
+  main: {
+    supportUrl: `${supportNewRequestUrl}?ticket-id=354522`,
+    getStateDescription: (vehicleName: string) => `Ton ${vehicleName} état:`,
+  },
+  address: {
+    getCarAddressTitle: (vehicleName: string) => `${getCommonAddressTitle(vehicleName)} (Auto)`,
+    getBusAddressTitle: (vehicleName: string) => `${getCommonAddressTitle(vehicleName)} (Bus)`,
+  }
+};
